a9a6b5a3ab6ec049400968745d0dae4c34cf5baf
pwnzf <pwnzforever@gmail.com>
2022-04-14 17:28:43 +0300

    image minimizer added

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const {extendDefaultPlugins} = require('svgo');
 
 module.exports = (env, argv) => ({
 	entry: ['@babel/polyfill', './src/index.js'],
@@ -11,10 +13,10 @@ module.exports = (env, argv) => ({
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name].[fullhash].js',
 	},
-	optimization: {
-		minimize: argv.mode === 'production',
-		minimizer: [new TerserPlugin()],
-	},
+	// Optimization: {
+	// 	minimize: argv.mode === 'production',
+	// 	minimizer: [new TerserPlugin()],
+	// },
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, 'src/index.html'),
@@ -68,6 +70,48 @@ module.exports = (env, argv) => ({
 					'style-loader', 'css-loader', 'postcss-loader', 'sass-loader',
 				],
 			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				type: 'asset',
+			},
+		],
+	},
+	optimization: {
+		minimize: argv.mode === 'production',
+		minimizer: [
+			new TerserPlugin(),
+			new ImageMinimizerPlugin({
+				minimizer: {
+					implementation: ImageMinimizerPlugin.imageminMinify,
+					options: {
+						// Lossless optimization with custom option
+						// Feel free to experiment with options for better result for you
+						plugins: [
+							['gifsicle', {interlaced: true}],
+							['jpegtran', {progressive: true}],
+							['optipng', {optimizationLevel: 5}],
+							// Svgo configuration here https://github.com/svg/svgo#configuration
+							[
+								'svgo',
+								{
+									plugins: extendDefaultPlugins([
+										{
+											name: 'removeViewBox',
+											active: false,
+										},
+										{
+											name: 'addAttributesToSVGElement',
+											params: {
+												attributes: [{xmlns: 'http://www.w3.org/2000/svg'}],
+											},
+										},
+									]),
+								},
+							],
+						],
+					},
+				},
+			}),
 		],
 	},
 	performance: {
